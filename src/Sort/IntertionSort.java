597152ed91228970aa3584fa775a625a90972a14
Keshav <keshavarun1220@gmail.com>
2025-05-23 10:56:01 +0530

    Add InsertionSort implementation in Java
    Introduce a new class, `IntertionSort`, which includes a static method for performing insertion sort on an integer array. The implementation sorts the array in-place and demonstrates its usage with a sample array in the `main` method.

---
+++ src/Sort/IntertionSort.java
@@ -0,0 +1,23 @@
+package Sort;
+
+import java.util.Arrays;
+
+public class IntertionSort {
+    public static void main(String[] args) {
+        int[] arr = {3,1,5,4,2};
+        insertionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void insertionSort(int[] arr){
+        for(int i=0; i< arr.length-1; i++){
+            for (int j = i+1; j >0 ; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                } else break;
+            }
+        }
+    }
+}
