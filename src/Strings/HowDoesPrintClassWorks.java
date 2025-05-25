5d8bc4eb4e2d63951db0866a06047a801b346b38
Keshav <keshavarun1220@gmail.com>
2025-05-25 12:45:02 +0530

    Add HowDoesPrintClassWorks example to Strings package
    Introduced a new class to demonstrate basic print statements in Java. Includes examples of printing integers and strings to the console.

---
+++ src/Strings/HowDoesPrintClassWorks.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class HowDoesPrintClassWorks {
+
+    public static void main(String[] args) {
+        System.out.println(56);
+        System.out.println("Keshav");
+        //System.out.println(56);
+
+    }
+}
