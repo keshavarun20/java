b5590a912dc1ed3dae1b5768a9d50420b34685e6
keshavarun20 <keshavarun1220@gmail.com>
2025-05-27 11:02:46 +0530

    Add pattern-generating classes to the Patterns package
    Introduced five Java classes (Pattern2 to Pattern6) to generate different patterns of stars and numbers. These classes demonstrate various nested loop techniques for pattern creation, enhancing the repository with useful examples for learning and reference.

---
+++ src/Patterns/Pattern6.java
@@ -0,0 +1,15 @@
+package Patterns;
+
+public class Pattern6 {
+    public static void main(String[] args) {
+        for (int i = 0; i <5 ; i++) {
+            for (int s=4;s>i;s--){
+                System.out.print(" ");
+            }
+            for (int j = 0; j <=i ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
