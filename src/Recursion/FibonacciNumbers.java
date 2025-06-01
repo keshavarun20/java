54ca21fb0d1e92c0682149431a8c8822921f5885
Keshav <keshavarun1220@gmail.com>
2025-06-01 12:03:06 +0530

    Add closed-form Fibonacci implementation using Binet's formula
    - Replaced recursive Fibonacci call with an efficient formula-based implementation.
    - Introduced `fibFormula` method to calculate Fibonacci numbers using doubles for precision.

--- src/Recursion/FibonacciNumbers.java
+++ src/Recursion/FibonacciNumbers.java
@@ -2,11 +2,21 @@ package Recursion;
 
 public class FibonacciNumbers {
     public static void main(String[] args) {
-        System.out.println(fib(3));
+        System.out.println(fibFormula(50));
     }
 
     static int fib(int n){
         if (n==0||n==1) return n;
         return fib(n-1) + fib(n-2);
+    }
+
+    static int fibFormula(int n) {
+        double sqrt5 = Math.sqrt(5);
+        double phi = (1 + sqrt5) / 2;
+
+        // Use double for calculation, then round to the nearest int
+        double fibN = (Math.pow(phi, n)) / sqrt5;
+
+        return (int) (fibN);
     }
 }
