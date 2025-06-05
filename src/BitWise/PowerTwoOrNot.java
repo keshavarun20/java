2d85d7f77c41494c35c9b2e3b70bd02a15122c6b
Keshav <keshavarun1220@gmail.com>
2025-06-05 12:36:05 +0530

    Implement `isPowerOFTwo` logic in `PowerTwoOrNot` class and add a basic test in `main` method

--- src/BitWise/PowerTwoOrNot.java
+++ src/BitWise/PowerTwoOrNot.java
@@ -1,4 +1,12 @@
 package BitWise;
 
 public class PowerTwoOrNot {
+    public static void main(String[] args) {
+        System.out.println(isPowerOFTwo(0));
+    }
+
+    static boolean isPowerOFTwo(int n){
+        return n> 0 && n == Integer.highestOneBit(n);
+        //return n> 0 && (n&(n-1)) ==0;
+    }
 }
