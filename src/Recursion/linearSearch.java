4af96cfeef0707db3663d86425f64cfdf99444b3
keshavarun20 <keshavarun1220@gmail.com>
2025-06-17 13:17:38 +0530

    Created Recursion function for Array List

--- src/Recursion/linearSearch.java
+++ src/Recursion/linearSearch.java
@@ -1,10 +1,11 @@
 package Recursion;
 
+import java.util.ArrayList;
+
 public class linearSearch {
     public static void main(String[] args) {
-        int arr[] = {1,2,3,18,9};
-        System.out.println(search(0,198,arr));
-        System.out.println(searchh(0,198,arr));
+        int arr[] = {1,2,3,18,18};
+       helper(arr,18,0);
     }
 
     static int search(int s, int target,int[] arr){
@@ -16,4 +17,24 @@ public class linearSearch {
         if (s >= arr.length) return false;
         return arr[s] == target || searchh(s+1,target,arr); // short-circuit: if found, stop
     }
+
+    static void helper(int[] arr, int target, int index) {
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> result = getAllIndex(arr, target, index, list);
+        System.out.println(result);
+        System.out.println(list);
+    }
+
+    static ArrayList<Integer> getAllIndex(int[] arr, int target, int index, ArrayList<Integer> list) {
+        if (index == arr.length) {
+            return list;
+        }
+
+        if (arr[index] == target) {
+            list.add(index);
+        }
+
+        return getAllIndex(arr, target, index + 1, list);
+    }
+
 }
