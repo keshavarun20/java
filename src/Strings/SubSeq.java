81a0dd536c4c2e05c797a2133547c6304859a271
keshavarun20 <keshavarun1220@gmail.com>
2025-06-28 14:30:57 +0530

    Updated Recursion method for Subsequence with ASCII Values of each character

--- src/Strings/SubSeq.java
+++ src/Strings/SubSeq.java
@@ -5,8 +5,8 @@ import java.util.List;
 
 public class SubSeq {
     public static void main(String[] args) {
-        subSeq("","abc");
-        System.out.println(subSeqA("","abc"));
+        subSeqToASCII("","abc");
+        //System.out.println(subSeqAA("","abc"));
     }
 
     static void subSeq(String p, String up){
@@ -32,4 +32,16 @@ public class SubSeq {
         left.addAll(right);
         return left;
     }
+
+    static void subSeqToASCII(String p, String up){
+        if (up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+        char ch = up.charAt(0);
+        subSeqToASCII(p + ch,up.substring(1));
+        subSeqToASCII(p,up.substring(1));
+        subSeqToASCII(p + (ch+0),up.substring(1));
+    }
+
 }
