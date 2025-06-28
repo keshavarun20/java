16473f6cafdb94ad3e1998aa6d87a1d7666c6348
keshavarun20 <keshavarun1220@gmail.com>
2025-06-28 15:20:32 +0530

    Iteration method for SubSet with Duplicates

--- src/Strings/SubSet.java
+++ src/Strings/SubSet.java
@@ -1,12 +1,13 @@
 package Strings;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class SubSet {
     public static void main(String[] args) {
-        int[] arr = {1,2,3};
-        System.out.println(subSet(arr));
+        int[] arr = {1,2,2};
+        System.out.println(subSetDuplicate(arr));
     }
     static List<List<Integer>> subSet(int[] arr){
         List<List<Integer>> outer = new ArrayList<>();
@@ -19,7 +20,34 @@ public class SubSet {
                 outer.add(internal);
             }
         }
+        return outer;
+    }
 
+    static List<List<Integer>> subSetDuplicate(int[] arr){
+        Arrays.sort(arr);
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+
+        int start = 0;
+        int end = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            start = 0;
+
+            // If the current number is the same as previous, start from end+1
+            if (i > 0 && arr[i] == arr[i - 1]) {
+                start = end + 1;
+            }
+
+            end = outer.size() - 1;
+            int size = outer.size();
+
+            for (int j = start; j < size; j++) {
+                List<Integer> internal = new ArrayList<>(outer.get(j));
+                internal.add(arr[i]);
+                outer.add(internal);
+            }
+        }
 
         return outer;
     }
