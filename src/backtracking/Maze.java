9ca3c831fe8568d43f7dc557ccac68a79609b937
keshavarun20 <keshavarun1220@gmail.com>
2025-07-09 14:56:19 +0530

    Added Diagonal approach for Maze

--- src/backtracking/Maze.java
+++ src/backtracking/Maze.java
@@ -1,9 +1,11 @@
 package backtracking;
 
+import java.util.ArrayList;
+
 public class Maze {
     public static void main(String[] args) {
         System.out.println(count(3,3));
-        path("",3,3);
+        System.out.println(pathDiagonal("",3,3));
     }
 
     static int count(int r , int c){
@@ -17,17 +19,43 @@ public class Maze {
         return left + right;
     }
 
-    static void path(String p,int r , int c){
+    static ArrayList<String> path(String p, int r , int c){
         if (r ==1 && c ==1){
-            System.out.println(p);
-            return;
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
         }
+
+        ArrayList<String> list =new ArrayList<>();
         if (r > 1) {
-            path(p+"D",r-1,c);
+            list.addAll(path(p+"D",r-1,c));
         }
         if (c>1){
-            path(p + "R",r,c-1);
+            list.addAll(path(p + "R",r,c-1));
         }
+
+        return list;
+    }
+
+    static ArrayList<String> pathDiagonal(String p, int r , int c){
+        if (r ==1 && c ==1){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        ArrayList<String> list =new ArrayList<>();
+        if (r > 1) {
+            list.addAll(pathDiagonal(p+"V",r-1,c));
+        }
+        if (c>1){
+            list.addAll(pathDiagonal(p + "H",r,c-1));
+        }
+        if (r >1 && c>1){
+            list.addAll(pathDiagonal(p + "D",r-1,c-1));
+        }
+
+        return list;
     }
 
 }
