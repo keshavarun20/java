678ecbbc0c7d948fbb5c645a7868639ef9057cdc
keshavarun20 <keshavarun1220@gmail.com>
2025-07-14 21:22:24 +0530

    Used Backtracking in all direction path

--- src/backtracking/Maze.java
+++ src/backtracking/Maze.java
@@ -7,9 +7,11 @@ public class Maze {
 //        System.out.println(count(3,3));
 //        System.out.println(path("",3,3));
 
-        boolean[][] board = {{true,true,true},{true,false,true},{true,true,true}};
+        boolean[][] board = {{true,true,true},{true,true,true},{true,true,true}};
+//
+//        pathWithRiver("",board,0,0);
 
-        pathWithRiver("",board,0,0);
+        AllPaths("",board,0,0);
 
     }
 
@@ -79,6 +81,33 @@ public class Maze {
         if (c< maze[0].length-1){
             pathWithRiver(p + 'R', maze, r, c+1 );
         }
+    }
+
+    static void AllPaths(String p,boolean[][] maze,int r,int c){
+        if (r == maze.length-1 && c == maze[0].length-1){
+            System.out.println(p);
+            return;
+        }
+
+        if (!maze[r][c]) return;
+
+        maze[r][c] = false;
+
+        if (r < maze.length-1) {
+            AllPaths(p + 'D', maze, r+1, c );
+        }
+
+        if (c< maze[0].length-1){
+            AllPaths(p + 'R', maze, r, c+1 );
+        }
+        if (r>0){
+            AllPaths(p + 'U', maze, r-1, c );
+        }
+        if (c>0){
+            AllPaths(p + 'L', maze, r, c-1 );
+        }
+
+        maze[r][c] = true;
     }
 
 }
