084a5f20a5f4daacf8635bc381119ed3e0910230
keshavarun20 <keshavarun1220@gmail.com>
2025-07-14 22:05:59 +0530

    Use Backtracking in all direction paths with printing the path Array

--- src/backtracking/Maze.java
+++ src/backtracking/Maze.java
@@ -1,6 +1,7 @@
 package backtracking;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Maze {
     public static void main(String[] args) {
@@ -10,8 +11,8 @@ public class Maze {
         boolean[][] board = {{true,true,true},{true,true,true},{true,true,true}};
 //
 //        pathWithRiver("",board,0,0);
-
-        AllPaths("",board,0,0);
+        int[][] path = new int[board.length][board[0].length];
+        allPathPrint("",board,0,0,path,1);
 
     }
 
@@ -108,6 +109,40 @@ public class Maze {
         }
 
         maze[r][c] = true;
+    }
+
+    static void allPathPrint(String p,boolean[][] maze,int r,int c,int[][] path, int step){
+        if (r == maze.length-1 && c == maze[0].length-1){
+            path[r][c]=step;
+            for (int [] arr : path){
+                System.out.println(Arrays.toString(arr));
+            }
+            System.out.println(p);
+            System.out.println();
+            return;
+        }
+
+        if (!maze[r][c]) return;
+
+        maze[r][c] = false;
+        path[r][c] =step;
+
+        if (r < maze.length-1) {
+            allPathPrint(p + 'D', maze, r+1, c,path,step+1);
+        }
+
+        if (c< maze[0].length-1){
+            allPathPrint(p + 'R', maze, r, c+1,path,step+1);
+        }
+        if (r>0){
+            allPathPrint(p + 'U', maze, r-1, c,path,step+1);
+        }
+        if (c>0){
+            allPathPrint(p + 'L', maze, r, c-1,path,step+1);
+        }
+
+        maze[r][c] = true;
+        path[r][c] =0;
     }
 
 }
